e52088ef8c2f87a62a377e41f36bda22b3f71a7a
Shawnbreak <thezhxin@gmail.com>
2026-04-04 21:48:48 +0800

    add sub.json to config multiple sub url

--- parse.py
+++ parse.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import os
 import sys
 from typing import List, Tuple
 import requests
@@ -19,35 +20,34 @@ logger.info("start parse")
 # https://github.com/chika0801/sing-box-examples
 config_temp = "./sing-box_1.13.json"
 config_result = "config.json"
+sub_config = "./sub.json"
+
 cache_file=".cache_content"
 cache_url=".cache_url"
+
+ai_exclude_tag_pattern = [ "香港", "台湾", "菲律宾" ]
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
 }
 
-def parse_args() -> Tuple[str, bool, bool, bool]:
+def parse_args() -> Tuple[bool, bool, bool]:
     parse = argparse.ArgumentParser(
         prog="parse",
         description="sing box parse"
     )
 
-    parse.add_argument("--url")
     parse.add_argument("--update", action='store_true', default=False)
     parse.add_argument("--tun", action='store_true', default=False)
     parse.add_argument("--mixed", action='store_true', default=False)
     
     args = parse.parse_args()
-    return args.url, args.update, args.tun, args.mixed
+    return args.update, args.tun, args.mixed
 
-def get_sub_raw(update: bool, url: str):
-    if url:
-        logger.info("cache url")
-        with open(cache_url, "w") as f:
-            f.write(url)
-
-    with open(cache_url, "r") as f:
-        url = f.read()
+def get_sub_raw(sub_name: str, url: str, update: bool):
+    cache_file = f".cache_{sub_name}"
+    if not os.path.exists(cache_file):
+        update = True
 
     if update:
         logger.info(f"update content from {url}")
@@ -174,15 +174,77 @@ def parse_lines(decode_lines: List[str]) -> Tuple[List[dict], List[str]]:
             outbound_tags.append(tag)
     return outbounds, outbound_tags
 
+def parse_sub(sub_name, sub_url, update):
+    
+    raw = get_sub_raw(sub_name, sub_url, update)
+    raw = check_pad(raw)
+    lines = decode_as_lines(raw)
+    outbounds, outbound_tags = parse_lines(lines)
+    return outbounds, outbound_tags
+    
+def main():
+    update, tun, mixed = parse_args()
+    with open(sub_config, "r") as f:
+        sub_config_content = f.read()
+        sub_json = json.loads(sub_config_content)
 
-def read_template(template_file: str) -> dict:
-    with open(template_file, "r") as f:
-        temp = json.loads(f.read())
-    return temp
+    subs = sub_json.get("subs")
 
+    outbounds = []
+    proxy_outbounds = []
+    ai_outbounds = []
+    subs_outbounds = []
 
-ai_exclude_tag_pattern = [ "香港", "台湾", "菲律宾" ]
-def fill_template(template: dict, outbounds: List[dict], outbound_tags: List[str], tun: bool, mixed: bool) -> dict:
+    for sub in subs:
+        if not sub.get("active"):
+            continue
+        sub_name = sub.get("name")
+        sub_url = sub.get("sub_url")
+        sub_outbounds, sub_outbound_tags = parse_sub(sub_name, sub_url, update)
+        subs_outbounds.append({
+            "type": "urltest",
+	    "tag": sub_name,
+	    "interval": "3m",
+	    "outbounds": [s.get("tag") for s in sub_outbounds]
+        })
+        for o in sub_outbounds:
+            subs_outbounds.append(o)
+        logger.info(f"{sub_name}: {len(sub_outbounds)}\n{[s.get('tag') for s in sub_outbounds]}")
+
+    proxy_outbounds.extend([s.get("tag") for s in subs_outbounds])
+    for s in subs_outbounds:
+        s_exclude = False
+        for e in ai_exclude_tag_pattern:
+            if e in s.get("tag"):
+                s_exclude = True
+                break
+            
+        if not s_exclude:
+            ai_outbounds.append(s.get("tag"))
+
+    outbounds.append({
+	"type": "direct",
+	"tag": "direct",
+	"domain_resolver": "local-dns"
+    })
+    
+    outbounds.append({
+        "type": "selector",
+	"tag": "proxy",
+	"outbounds": proxy_outbounds
+    })
+
+    outbounds.append({
+	"type": "selector",
+	"tag": "ai",
+	"outbounds": ai_outbounds
+    })
+
+    outbounds.extend(subs_outbounds)
+
+    with open(config_temp, "r") as f:
+        template = json.loads(f.read())
+    
     temp_inbounds = template["inbounds"]
     inbounds = []
     for i in temp_inbounds:
@@ -191,50 +253,11 @@ def fill_template(template: dict, outbounds: List[dict], outbound_tags: List[str
         if i["type"] == "mixed" and mixed:
             inbounds.append(i)
     template["inbounds"] = inbounds
-    temp_outbounds = template["outbounds"]
-    for o in temp_outbounds:
-        if o["tag"] == "proxy":
-            o["outbounds"] = ["auto"]
-            for t in  outbound_tags:
-                o["outbounds"].append(t)
+    template["outbounds"] = outbounds
 
-        if o["tag"] == "auto":
-            o["outbounds"] = outbound_tags
-        if o["tag"] == "ai":
-            ai_tags = []
-            for t in outbound_tags:
-                t_ok = True
-                for et in ai_exclude_tag_pattern:
-                    if et in t:
-                        t_ok = False
-                        break
-                if t_ok:
-                    ai_tags.append(t)
-
-            o["outbounds"] = ai_tags
-
-    for o in outbounds:
-        temp_outbounds.append(o)
-
-    return template
-
-def write_config(config_result: str, template: dict):
-    result = json.dumps(template, indent=4, ensure_ascii=False)
     with open(config_result, "w") as f:
-        f.write(result)
-
-def main():
-    url, update, tun, mixed = parse_args()
-    raw = get_sub_raw(update, url)
-    raw = check_pad(raw)
-    lines = decode_as_lines(raw)
-    outbounds, outbound_tags = parse_lines(lines)
-    count = len(outbound_tags)
-    logger.info(outbound_tags)
-    logger.info(f"total nodes: {count}")
-    template = read_template(config_temp)
-    result = fill_template(template, outbounds, outbound_tags, tun, mixed)
-    write_config(config_result, result)
+        f.write(json.dumps(template, indent=4, ensure_ascii=False))
+        
 
 if __name__ == "__main__":
     main()
